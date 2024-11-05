0cc5fc95a28f41b46e0e087cfd5d2ead38cff96f
John5536 <rrivers52@gmail.com>
2024-11-05 16:19:20 -0500

    updates located in read me

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-let user_Input1 = document.getElementById('user_input1')
-let plant_list = document.getElementById('output1')
+//let user_Input1 = document.getElementById('user_input1')
+//let plant_list = document.getElementById('output1')
+
+function new_plants(){
+    let plant_added = user_input1.value;
+    document.getElementById("output1").innerHTML+=`${plant_added} `;
+}
